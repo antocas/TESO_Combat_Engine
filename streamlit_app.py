21d5025883f4f4a3ca5fe9dab26905a0e98d643b
Antonio Castillo Diaz <acastillo@trevenque.es>
2022-04-27 12:12:09 +0200

    Added dynamic metrics, just a mock

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from src.Visual_Components.character_card import generate_character_card
 from src.Visual_Components.dummy_card import generate_dummy_card
 
+from main import main_combat_test
 
 def sidebar_block():
     return st.sidebar.selectbox("Options", ('Dummy', 'Character', 'Skills'))
@@ -10,11 +11,12 @@ def sidebar_block():
 def dps_metric():
     columns = st.columns([2, 2, 6, 2])
     with columns[0]:
-        st.metric(label="DPS", value="10k")
+        st.metric(label="DPS", value=st.session_state.get('dps') or 0)
     with columns[1]:
-        st.metric(label="Time", value="12 sec")
+        st.metric(label="Time (seconds)", value=st.session_state.get('time') or 0)
     with columns[3]:
-        st.button('Calculate')
+        if st.button('Calculate', on_click=main_combat_test):
+            st.experimental_rerun()
 
 if __name__ == '__main__':
     st.set_page_config(layout="wide")
