3d2a0f8e4d207003dccd09cda6530d8b2ce06307
Antonio Castillo <acastillo@trevenque.es>
2022-06-08 16:27:07 +0200

    Hotfix, hopefully last one

--- src/visual/rotation_card.py
+++ src/visual/rotation_card.py
@@ -10,28 +10,29 @@ def generate_rotation_card():
     main_abilities = set()
     second_abilities = set()
 
-    with cols[0]:
-        main_abilities.add(st.selectbox('main bar #1', st.session_state['skills_selected']['skills']))
-        second_abilities.add(st.selectbox('second bar #1', st.session_state['skills_selected']['skills']))
-    with cols[1]:
-        main_abilities.add(st.selectbox('main bar #2', st.session_state['skills_selected']['skills']))
-        second_abilities.add(st.selectbox('second bar #2', st.session_state['skills_selected']['skills']))
-    with cols[2]:
-        main_abilities.add(st.selectbox('main bar #3', st.session_state['skills_selected']['skills']))
-        second_abilities.add(st.selectbox('second bar #3', st.session_state['skills_selected']['skills']))
-    with cols[3]:
-        main_abilities.add(st.selectbox('main bar #4', st.session_state['skills_selected']['skills']))
-        second_abilities.add(st.selectbox('second bar #4', st.session_state['skills_selected']['skills']))
-    with cols[4]:
-        main_abilities.add(st.selectbox('main bar #5', st.session_state['skills_selected']['skills']))
-        second_abilities.add(st.selectbox('second bar #5', st.session_state['skills_selected']['skills']))
-    with cols[5]:
-        main_ultimate = st.selectbox('main ultimate', st.session_state['skills_selected']['ultimate'])
-        second_ultimate = st.selectbox('second ultimate', st.session_state['skills_selected']['ultimate'])
+    if st.session_state.get('skills_selected'):
+        with cols[0]:
+            main_abilities.add(st.selectbox('main bar #1', st.session_state['skills_selected']['skills']))
+            second_abilities.add(st.selectbox('second bar #1', st.session_state['skills_selected']['skills']))
+        with cols[1]:
+            main_abilities.add(st.selectbox('main bar #2', st.session_state['skills_selected']['skills']))
+            second_abilities.add(st.selectbox('second bar #2', st.session_state['skills_selected']['skills']))
+        with cols[2]:
+            main_abilities.add(st.selectbox('main bar #3', st.session_state['skills_selected']['skills']))
+            second_abilities.add(st.selectbox('second bar #3', st.session_state['skills_selected']['skills']))
+        with cols[3]:
+            main_abilities.add(st.selectbox('main bar #4', st.session_state['skills_selected']['skills']))
+            second_abilities.add(st.selectbox('second bar #4', st.session_state['skills_selected']['skills']))
+        with cols[4]:
+            main_abilities.add(st.selectbox('main bar #5', st.session_state['skills_selected']['skills']))
+            second_abilities.add(st.selectbox('second bar #5', st.session_state['skills_selected']['skills']))
+        with cols[5]:
+            main_ultimate = st.selectbox('main ultimate', st.session_state['skills_selected']['ultimate'])
+            second_ultimate = st.selectbox('second ultimate', st.session_state['skills_selected']['ultimate'])
 
-    list_of_skills = list(main_abilities) + list(second_abilities) + [main_ultimate] + [second_ultimate]
-    order = st.multiselect("Orden de habilidades", list_of_skills)
-    if order is None:
-        order = []
-    st.session_state['rotation_selected'] = order
-    st.write(', '.join(order))
+        list_of_skills = list(main_abilities) + list(second_abilities) + [main_ultimate] + [second_ultimate]
+        order = st.multiselect("Orden de habilidades", list_of_skills)
+        if order is None:
+            order = []
+        st.session_state['rotation_selected'] = order
+        st.write(', '.join(order))
